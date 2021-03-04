7dcb45811d0de892f5362a2ebbe6a795fe50d66e
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-03 21:35:46 -0500

    commit tree 83a872addc0170c08847e5b53d69f919c48f8000

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+dwarves.each.with_index(1) {|name, index| puts "#{index}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(words)# code an argument here
   # Your code here
+words.collect {|cap| cap.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
+   if calls_long.any? {|element| element.size > 4}
+    return true
+   else
+    return false
+end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+ cheddar_cheese.find {|cheese| cheese_types.include?(cheese)}
 end
